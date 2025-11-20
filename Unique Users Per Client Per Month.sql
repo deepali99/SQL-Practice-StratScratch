7ac06bed595d87c2b090f8c59e1104fe0746f3bd
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-11-20 09:02:22 -0800

    Create query for unique users per client per month
    Add SQL query to count unique users per client per month.

---
+++ Unique Users Per Client Per Month.sql
@@ -0,0 +1,13 @@
+/*
+https://platform.stratascratch.com/coding/2024-unique-users-per-client-per-month/official-solution?code_type=3
+Write a query that returns the number of unique users per client for each month. Assume all events occur within the same year, so only month needs to be be in the output as a number from 1 to 12.
+*/
+
+
+
+SELECT client_id,
+       MONTH(time_id) as month,
+       COUNT(DISTINCT user_id) as users_num
+FROM fact_events
+GROUP BY client_id,
+          MONTH(time_id)
