345e8d9e6ad9094184de156bf0be7f537b13d5c5
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-11-23 13:33:29 -0800

    Implement ranking of guests based on age
    Add SQL query to rank guests by age in descending order.

---
+++ Rank guests based on their ages.sql
@@ -0,0 +1,13 @@
+/*
+
+Rank guests based on their ages.
+Output the guest id along with the corresponding rank.
+Order records by the age in descending order.
+
+
+*/
+
+SELECT 
+    guest_id, 
+    RANK()OVER(ORDER BY age DESC) AS rnk
+FROM airbnb_guests
